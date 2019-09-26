050cb082a3c39b19236c112b50bf929b5ac06340
mads758e mads758e <mads758e@edu.campusvejle.dk>
2019-09-26 11:07:37 +0200

    adding encapsulation to the stats.cs file

--- InheritenceOfMasterRace/Stats.cs
+++ InheritenceOfMasterRace/Stats.cs
@@ -23,6 +23,7 @@ namespace InheritenceOfMasterRace
         protected byte constitution;
         protected byte wisdom;
         protected byte charisma;
+        protected byte maxStat = 250;
 
         //Constructors
         public Stats(string name, string lastName, byte hP, byte strenght, byte inteligens, byte agility, byte stamina, byte constitution, byte wisdom, byte charisma)
@@ -30,29 +31,162 @@ namespace InheritenceOfMasterRace
             Name = name;
             LastName = lastName;
             HP = hP;
-            Strenght = strenght;
-            Inteligens = inteligens;
+            Strength = strenght;
+            Intelligence = inteligens;
             Agility = agility;
             Stamina = stamina;
             Constitution = constitution;
             Wisdom = wisdom;
             Charisma = charisma;
+
         }
 
         //Properties
         public string Name { get; set; }
         public string LastName { get; set; }
-        public byte HP { get; set; }
-        public byte Mana { get; set; }
-        public byte Strenght { get; set; }
-        public byte Inteligens { get; set; }
-        public byte Agility { get; set; }
-        public byte Stamina { get; set; }
-        public byte Constitution { get; set; }
-        public byte Wisdom { get; set; }
-        public byte Charisma { get; set; }
+        public byte HP
+        {
+            get { return hP; }
+            set
+            {
+                if (hP < maxStat)
+                {
+                    hP = value;
+                }
 
-        
+                if (hP >= maxStat)
+                {
+                    hP = maxStat;
+                }
+            }
+        }
+
+        public byte Strength
+        {
+            get { return strenght; }
+            set
+            {
+                if (strenght < maxStat)
+                {
+                    strenght = value;
+                }
+
+                if (strenght >= maxStat)
+                {
+                    strenght = maxStat;
+                }
+            }
+        }
+
+        public byte Intelligence
+        {
+            get { return inteligens; }
+            set
+            {
+                if (inteligens < maxStat)
+                {
+                    inteligens = value;
+                }
+
+                if (inteligens >= maxStat)
+                {
+                    inteligens = maxStat;
+                }
+            }
+        }
+
+        public byte Agility
+        {
+            get { return agility; }
+            set
+            {
+                if (agility < maxStat)
+                {
+                    agility = value;
+                }
+
+                if (agility >= maxStat)
+                {
+                    agility = maxStat;
+                }
+            }
+        }
+
+        public byte Stamina
+        {
+            get
+            {
+                return stamina;
+            }
+            set
+            {
+                if (stamina < maxStat)
+                {
+                    stamina = value;
+                }
+                if (stamina <= maxStat)
+                {
+                    stamina = maxStat;
+                }
+            }
+        }
+        public byte Constitution
+        {
+            get
+            {
+                return constitution;
+            }
+            set
+            {
+                if (constitution < maxStat)
+                {
+                    constitution = value;
+                }
+                if (constitution <= maxStat)
+                {
+                    constitution = maxStat;
+                }
+            }
+        }
+        public byte Wisdom
+        {
+            get
+            {
+                return wisdom;
+            }
+            set
+            {
+                if (wisdom < maxStat)
+                {
+                    wisdom = value;
+                }
+                if (wisdom <= maxStat)
+                {
+                    wisdom = maxStat;
+                }
+            }
+        }
+        public byte Charisma
+        {
+            get
+            {
+                return charisma;
+            }
+            set
+            {
+                if (charisma < maxStat)
+                {
+                    charisma = value;
+                }
+                if (charisma <= maxStat)
+                {
+                    charisma = maxStat;
+                }
+            }
+        }
+
+
+
 
     }
 }
